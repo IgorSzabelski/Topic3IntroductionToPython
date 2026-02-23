585e35273f7da3c009bd4f097a8ee9da26168160
Igor <szabelskiigor@gmail.com>
2026-02-23 13:28:41 +0100

    Cleanup

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+# Tutorial from https://docs.python.org/3/tutorial/index.html
+# Chapter 3: An Informal Introduction to Python https://docs.python.org/3/tutorial/introduction.html
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def some_function():
+    return  # TODO
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    some_function()
