4c043a3d9865a7c123a76bbd38e881d81ba1c4b7
Igor <szabelskiigor@gmail.com>
2026-02-23 13:31:56 +0100

    [3.] Comments vs not comments

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # Tutorial from https://docs.python.org/3/tutorial/index.html
 # Chapter 3: An Informal Introduction to Python https://docs.python.org/3/tutorial/introduction.html
 
-def some_function():
-    return  # TODO
+def comments():
+    spam = 1  # this is a comment
+    text = "# this is not a comment because it's inside quotes"
+    print(spam)
+    print(text)
 
 
 if __name__ == '__main__':
-    some_function()
+    comments()
